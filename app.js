278218c92b5a5b4d269d30f4a1aec11e5685b452
Subhajit Das <das840336@gmail,com>
2026-08-24 16:03:04 +0530

    Fix server port for deployment

--- app.js
+++ app.js
@@ -17,20 +17,17 @@ const indexRouter = require('./routes/index');
 // MongoDB connection
 require('./config/mongoose-connection');
 
-// ===============================
 // Middleware
-// ===============================
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(
-  expressSession({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.EXPRESS_SESSION_SECRET || 'secret'
-  })
+    expressSession({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.EXPRESS_SESSION_SECRET || 'secret'
+    })
 );
 
 app.use(flash());
@@ -41,22 +38,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 // EJS
 app.set('view engine', 'ejs');
 
-// ===============================
 // Routes
-// ===============================
-
 app.use('/', indexRouter);
 app.use('/owner', ownerRouter);
 app.use('/users', usersRouter);
 app.use('/products', productRouter);
 
-// ===============================
 // Server
-// ===============================
-
-// IMPORTANT FOR RENDER
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`SHOES E-Commerce Server Running on Port ${PORT}`);
+    console.log(`SHOES E-Commerce Server Running on Port ${PORT}`);
 });
