5de4d1107e218dab011993d6c6c22a9dab329dee
Subhajit Das <das840336@gmail,com>
2026-09-23 11:46:48 +0530

    Fix MongoDB environment loading

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+console.log("MONGODB_URI exists:", !!process.env.MONGODB_URI);
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((err) => {
-    console.log('MongoDB connection error:', err);
+    console.log("MongoDB connection error:", err);
   });
 
 module.exports = mongoose.connection;
